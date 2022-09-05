ccb7ad7e986c14549e3aca88ecaed71f471065c4
Ramzi Hemadou <ramzi.hemadou@flambosoft.com>
2022-09-05 15:24:24 +0100

    add post, turbo

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get '/notice', to: 'posts#clear_message'
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "posts#index"
 end
